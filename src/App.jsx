eff2e96ce72e0723f8d06c030508d6ae500835e1
iandominic <javalavama@gmail.com>
2024-01-01 22:48:46 +0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import logo from "../src/assets/image-avatar.webp";
 const App = () => {
   return (
     <div className="w-full h-screen bg-main flex justify-center items-center">
-      <div className="bg-white z-10 relative shad w-[320px] h-[490px] rounded-2xl flex flex-col  overflow-hidden p-[22px] border-black border-[1px]">
+      <div className="bg-white z-10 relative shad w-[320px] h-[490px] md:h-[510px] md:w-[340px] rounded-2xl flex flex-col  overflow-hidden p-[22px] border-black border-[1px]">
         <div className="w-full h-[195px] overflow-hidden rounded-xl ">
-          <img className="w-full scale-[1.2] h-[193px]" src={article} />
+          <img alt="" className="w-full scale-[1.2] h-[193px]" src={article} />
         </div>
         <div className="flex pt-6 pb-[11px] gap-3 flex-col">
           <div className="text-[12px] flex items-center justify-center rounded-md max-w-[72px] text-center bg-main px-3 py-1  font-bold">
@@ -24,7 +24,7 @@ const App = () => {
             structure, content, and presentation.
           </p>
           <div className="flex pt-[12px] items-center gap-3">
-            <img width={30} src={logo} />
+            <img alt="" width={30} src={logo} />
             <p className="m-0 p-0 font-bold text-[14px]">Greg Hooper</p>
           </div>
         </div>
@@ -32,5 +32,4 @@ const App = () => {
     </div>
   );
 };
-
 export default App;
